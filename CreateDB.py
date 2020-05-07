30f8cc599cd90c96305868b3a8061df3821b2318
Ohclemintine21 <60559963+Ohclemintine21@users.noreply.github.com>
2020-05-07 11:18:13 -0700

    Corrected DB drill assignment

--- CreateDB.py
+++ CreateDB.py
@@ -1,47 +1,25 @@
 import sqlite3
 
-conn = sqlite3.connect('testFind.db')
 
+conn = sqlite3.connect('textFiles.db')
+#create and clear db
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_fname TEXT \
         )")
-    cur.execute("DELETE FROM tbl_files WHERE true")
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('textFiles.db')
+
+fileList = ('information.docx','Hello.txt','myImage.png', 'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+for file in fileList:
+    if ('.txt' in file):
+        print(file)
     conn.commit()
 conn.close()
 
 
-conn = sqlite3.connect('testFind.db')
 
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('information.docx',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('Hello.txt',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myImage.png',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myMovie.mpg',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('World.txt',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('data.pdf',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myPhoto.jpg',))
-    
-    conn.commit()
-conn.close()
-
-conn = sqlite3.connect('testFind.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_fname FROM tbl_files WHERE col_fname LIKE '%.txt'")
-    varFile = cur.fetchall()
-    for item in varFile:
-        msg = "File Name: {}".format(item[0])
-        print(msg)
-    
